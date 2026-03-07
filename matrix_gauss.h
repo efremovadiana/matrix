0b5a3f99450fcfd41a128b7b4a7687cb5fcb55d1
efremovadiana <136587564+efremovadiana@users.noreply.github.com>
2026-03-07 22:06:59 +0300

    Update matrix_gauss.h

--- matrix_gauss.h
+++ matrix_gauss.h
@@ -5,6 +5,7 @@
 
 /* Решение СЛАУ методом Гаусса, A -  матрица коэффициентов (квадратная), B - мтарица правых частей
  * tol - допуск для определения вырожденности
+ * return - матрица X решений (нужно освободить через matrix_free) или NULL при ошибке
  */
 
 matrix *matrix_solve_gauss(const matrix *A, const matrix *B, double tol);
@@ -17,3 +18,4 @@ matrix *matrix_solve_gauss(const matrix *A, const matrix *B, double tol);
 double matrix_check_solution(const matrix *A, const matrix *B, const matrix *X);
 
 #endif // MATRIX_GAUSS_H_INCLUDED
+
